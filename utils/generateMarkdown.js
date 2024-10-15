ef50c010f84a89e7cda90a8329db6594b2b0fadf
Michael Fleming <mikahl171@gmail.com>
2024-10-15 16:04:16 -0500

    still tyring to push generatemarkdown changes

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,38 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'None' || !license) {
+    return ''; 
+  }
+
+  const badges = {
+    'Apache2.0': 'https://img.shields.io/badge/license-Apache2.0-brightgreen',
+    'Boost1.0': 'https://img.shields.io/badge/license-Boost1.0-brightgreen',
+    'BSD': 'https://img.shields.io/badge/license-BSD-brightgreen',
+    'Eclipse': 'https://img.shields.io/badge/license-Eclipse-brightgreen',
+    'MIT': 'https://img.shields.io/badge/license-MIT-brightgreen'
+  };
+
+  return `![License](${badges[license]})`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'None' || !license) {
+    return '';
+  }
+
+  const licenseLinks = {
+    'Apache2.0': 'https://www.apache.org/licenses/LICENSE-2.0',
+    'Boost1.0': 'https://www.boost.org/LICENSE_1_0.txt',
+    'BSD': 'https://opensource.org/licenses/BSD-3-Clause',
+    'Eclipse': 'https://www.eclipse.org/legal/epl-2.0/',
+    'MIT': 'https://opensource.org/licenses/MIT'
+  };
+
+  return licenseLinks[license];
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
